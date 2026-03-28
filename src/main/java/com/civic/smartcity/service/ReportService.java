f9fc2794c22cbdc315c17aa7fb8223b2d41c1fc8
Sai srujana thanneru <saisrujanathanneru@gmail.com>
2026-03-28 20:12:37 +0530

    Create ReportService.java

---
+++ src/main/java/com/civic/smartcity/service/ReportService.java
@@ -0,0 +1,11 @@
+package com.civic.smartcity.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+
+    public String generateReport() {
+        return "CivicPulseHub Report Generated Successfully!";
+    }
+}
